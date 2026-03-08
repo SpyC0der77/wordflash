2abed448d96e90d7a351e8f209fe86f7fd3a2f30
SpyC0der77 <carter.stach@gmail.com>
2026-03-08 14:40:16 -0400

    Initialize mobile app

---
+++ apps/ios/WordFlash/WordFlash/WordFlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordFlashApp.swift
+//  WordFlash
+//
+//  Created by Carter Stach on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct WordFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps/ios/WordFlash/WordFlashTests/WordFlashTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordFlashTests.swift
+//  WordFlashTests
+//
+//  Created by Carter Stach on 3/8/26.
+//
+
+import Testing
+@testable import WordFlash
+
+struct WordFlashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
